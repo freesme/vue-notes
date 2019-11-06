fec919171c2d660cdf466ffde9ab8f425d138044
lemon <freesme@163.com>
2019-11-06 22:09:47 +0800

    封装tarbar init

---
+++ vuejs-v2/tabbar/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div>首页</div>
+    <div>分类</div>
+    <div>购物车</div>
+    <div>我的</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
